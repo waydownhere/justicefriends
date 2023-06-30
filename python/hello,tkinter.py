d79d865d4d6e3f706aa5511be6b5889dd0f301a8
waydownhere <101130640+waydownhere@users.noreply.github.com>
2023-06-30 13:11:26 -0400

    Starting on the Tkinter`

--- python/hello,tkinter.py
+++ python/hello,tkinter.py
@@ -1,8 +1,35 @@
 from tkinter import *
 from tkinter import ttk
+
 root = Tk()
-frm = ttk.Frame(root, padding=10)
+frm = ttk.Frame(root, padding=100)
 frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
+
+# Create a StringVar for updating the label text
+counter = StringVar()
+counter.set("0")
+
+#label that displays "victory points" above the two buttons
+ttk.Label(frm, text="Victory Points").grid(column=0, row=0)
+
+# function to increment the counter
+def increment():
+    counter.set(str(int(counter.get()) + 1))
+
+# function to decrement the counter
+def decrement():
+    counter.set(str(int(counter.get()) - 1 if int(counter.get()) > 0 else 0))
+
+#button that increases a counter when clicked
+ttk.Button(frm, text="Add", command=increment).grid(column=1, row=0)
+
+# button that decreases a counter when clicked
+ttk.Button(frm, text="Subtract", command=decrement).grid(column=2, row=0)
+
+# label that displays the counter
+ttk.Label(frm, textvariable=counter).grid(column=3, row=0)
+
+# maximize the window
+root.state('zoomed')
+
 root.mainloop()
