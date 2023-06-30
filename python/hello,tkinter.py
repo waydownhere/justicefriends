7fc3b8b5034bd3cd1bba8328a73be1f3c9a3d111
waydownhere <101130640+waydownhere@users.noreply.github.com>
2023-06-30 13:30:33 -0400

    Turns out I was making a VP counter, who knew.

--- python/hello,tkinter.py
+++ python/hello,tkinter.py
@@ -3,33 +3,32 @@ from tkinter import ttk
 
 root = Tk()
 frm = ttk.Frame(root, padding=100)
-frm.grid()
+frm.pack()
+
+#Label that displays "Victory Points Counter" in the window heading
+root.title("Victory Points Counter")
 
 # Create a StringVar for updating the label text
 counter = StringVar()
 counter.set("0")
 
-#label that displays "victory points" above the two buttons
-ttk.Label(frm, text="Victory Points").grid(column=0, row=0)
-
-# function to increment the counter
-def increment():
-    counter.set(str(int(counter.get()) + 1))
-
-# function to decrement the counter
-def decrement():
-    counter.set(str(int(counter.get()) - 1 if int(counter.get()) > 0 else 0))
+# Frame for the buttons
+button_frame = ttk.Frame(frm)
+button_frame.pack()
 
 #button that increases a counter when clicked
-ttk.Button(frm, text="Add", command=increment).grid(column=1, row=0)
+ttk.Button(button_frame, text="Add", command=lambda: counter.set(str(int(counter.get()) + 1))).pack(side=LEFT)
 
 # button that decreases a counter when clicked
-ttk.Button(frm, text="Subtract", command=decrement).grid(column=2, row=0)
+ttk.Button(button_frame, text="Subtract", command=lambda: counter.set(str(int(counter.get()) - 1 if int(counter.get()) > 0 else 0))).pack(side=LEFT)
+
+# Button that resets the counter when clicked, positioned in the upper right corner
+ttk.Button(button_frame, text="Reset", command=lambda: counter.set("0")).pack(side=LEFT)
+
+#label that displays "Victory Points"
+ttk.Label(frm, text="Victory Points", font=("Helvetica", 20)).pack()
 
 # label that displays the counter
-ttk.Label(frm, textvariable=counter).grid(column=3, row=0)
-
-# maximize the window
-root.state('zoomed')
+ttk.Label(frm, textvariable=counter, font=("Helvetica", 50, "bold" )).pack()
 
 root.mainloop()
